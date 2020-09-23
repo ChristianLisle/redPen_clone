39baa349240bac27e188d0447f9ace7a3e44dd53
jonahd <jonahd@iastate.edu>
2020-09-22 23:42:07 -0500

    added lines to MainActivity

--- Frontend/app/src/main/java/com/example/redpen/utils/MainActivity.java
+++ Frontend/app/src/main/java/com/example/redpen/utils/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.redpen.utils;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.redpen.R;
 
@@ -12,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnJsonObject = (Button) findViewById(R.id.button_string_array);
+        Button btnJsonArray = (Button) findViewById(R.id.button_string_object);
+
+
+
+
+
     }
 }
