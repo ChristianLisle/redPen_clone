150055f36ddea56a23a4d247994d294c20a486fc
jonahd <jonahd@iastate.edu>
2020-09-22 23:26:44 -0500

    Removed image requests from AppController

--- Frontend/app/src/main/java/com/example/redpen/app/AppController.java
+++ Frontend/app/src/main/java/com/example/redpen/app/AppController.java
@@ -5,9 +5,7 @@ import android.text.TextUtils;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
-import com.example.sumon.androidvolley.utils.LruBitmapCache;
 
 public class AppController extends Application {
 
@@ -15,7 +13,7 @@ public class AppController extends Application {
 			.getSimpleName();
 
 	private RequestQueue mRequestQueue;
-	private ImageLoader mImageLoader;
+
 
 	private static AppController mInstance;
 
@@ -37,14 +35,7 @@ public class AppController extends Application {
 		return mRequestQueue;
 	}
 
-	public ImageLoader getImageLoader() {
-		getRequestQueue();
-		if (mImageLoader == null) {
-			mImageLoader = new ImageLoader(this.mRequestQueue,
-					new LruBitmapCache());
-		}
-		return this.mImageLoader;
-	}
+
 
 	public <T> void addToRequestQueue(Request<T> req, String tag) {
 		// set the default tag if tag is empty
