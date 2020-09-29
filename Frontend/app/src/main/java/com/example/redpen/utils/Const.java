4684a194b63eb9adcc3e9fc037b1fa882ab3b736
jonahd <jonahd@iastate.edu>
2020-09-29 13:07:42 -0500

    Added parent2student.json url to Const.java

--- Frontend/app/src/main/java/com/example/redpen/utils/Const.java
+++ Frontend/app/src/main/java/com/example/redpen/utils/Const.java
@@ -4,5 +4,6 @@ public class Const {
 
     public static final String URL_USERS = "http://coms-309-ug-05.cs.iastate.edu:8080/users.json";
     public static final String URL_ASSIGNMENTS = "http://coms-309-ug-05.cs.iastate.edu:8080/assignments.json";
+    public static final String URL_PARENT_2_STUDENT = "http://coms-309-ug-05.cs.iastate.edu:8080/parent2student.json";
 
 }
