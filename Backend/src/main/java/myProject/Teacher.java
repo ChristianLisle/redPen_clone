9e4f1d2383bed418394eb3acc3785fd616909bd7
Carter Moseley <cmoseley@iastate.edu>
2020-10-04 22:03:14 -0500

    add the Teacher and Teacher Repository files

---
+++ Backend/src/main/java/myProject/Teacher.java
@@ -0,0 +1,43 @@
+package myProject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+import javax.persistence.*;
+
+
+@Entity
+public class Teacher {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    
+    @Column
+    String name;
+    
+    //@OneToMany(mappedBy = "teacher")
+    //Set<CourseRegistration> registrations;
+    
+    //@OneToMany(mappedBy = "teacher")
+    //Set<AssignedAssignment> assignments;
+    
+    public Teacher()	{}
+    
+    public Teacher(String name) {
+    	this.name = name;
+    }
+ 
+    public Integer getId()	{
+    	return id;
+    }
+
+    public String getName()	{
+    	return name;
+    }
+    
+    public void setName(String name)	{
+    	this.name = name;
+    }
+    
+}
